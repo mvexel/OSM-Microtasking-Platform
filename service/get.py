1989b1a56e50655eaf30d14e6ae35d7c879dbfcc
Martijn van Exel <mvexel@gmail.com>
2012-03-19 23:55:17 -0600

    made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.made it work in IE, added some niceties.

--- service/get.py
+++ service/get.py
@@ -3,21 +3,24 @@ import psycopg2
 import geojson
 
 urls = (
-    '/.*', 'getcandidate'
+    '/(.*)', 'getcandidate'
 )
 
 app = web.application(urls, globals(), autoreload=False)
 application = app.wsgifunc()
 
 class getcandidate:        
-    def GET(self):
+    def GET(self,nbi_id):
         conn = psycopg2.connect("host=localhost dbname=bexp user=osm password=osm")
         cur = conn.cursor()
-        cur.execute("SELECT ST_X(the_geom),ST_Y(the_geom), nbi_id FROM nbi_nonmatch ORDER BY RANDOM() LIMIT 1")
+        if nbi_id:
+            cur.execute("SELECT ST_X(the_geom),ST_Y(the_geom), nbi_id FROM nbi_nonmatch WHERE nbi_id = %s", (nbi_id))
+        else:
+            cur.execute("SELECT ST_X(the_geom),ST_Y(the_geom), nbi_id FROM nbi_nonmatch ORDER BY RANDOM() LIMIT 1")
         recs = cur.fetchall()
         (x,y,nbi_id) = recs[0]
         p=geojson.Point([x,y])
-        f=geojson.Feature(geometry=p,id=nbi_id)
+        f=geojson.Feature(geometry=p,properties={"nbi_id": nbi_id})
         return geojson.dumps(f)
 
 if __name__ == "__main__":
